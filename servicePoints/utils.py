05de56b6f09ab80a7e37d85825275f88f8943a90
Allie Bopp <aebopp@umich.edu>
2020-08-13 01:16:51 -0400

    adding in utils

---
+++ servicePoints/utils.py
@@ -0,0 +1,12 @@
+"""Utils file.
+
+This file is to house code common between the Master and the Worker
+
+"""
+
+def key():
+    return b'''\xf4\xb2\x9f\x84\xb1\xef\x01\xc6\x10\xca
+    \xdd\x84\xd4\xf3\x0c\x85\xad\xa6\xdc\xaf\xd3\xbeI\xf7'''
+
+def password():
+    return 'srvcpnts2020!'
